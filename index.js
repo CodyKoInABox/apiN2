d9f4d81660c62f21b87f2a0a3048d4c4fbad02ff
Cody Ko In A Box <125526050+CodyKoInABox@users.noreply.github.com>
2023-05-12 12:08:48 -0300

    superfactorial is now calculated with recursion

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const cors = require('cors')
 
 app.use(cors())
 
+
 app.get('/factorial', (req, res) =>{
     let value = parseInt(req.query.value)
   
@@ -12,27 +13,16 @@ app.get('/factorial', (req, res) =>{
 
 })
 
+
 app.get('/superfactorial', (req, res) => {
     let value = parseInt(req.query.value)
 
-    let allResults = [];
-
-    //push the factorial of each value from 1 to the user's input
-    for(let i = 1; i <= value; i++){
-        allResults.push(factorial(i))
-    }
-
-    //calculate the product of all of the factorials of all the numbers between 1 and the user's input
-    let result = 1;
-    for(let i = 0; i < allResults.length; i++){
-        result = result * allResults[i]
-    }
-
     res.status(200).send({
-        'result': result.toString()
+        'result': superFactorial(value).toString()
     })
 
 })
+
 
 //host the API on port 8080
 app.listen(8080, () =>{
@@ -47,5 +37,16 @@ function factorial(x){
     }
     else{
         return x * factorial(x-1)
+    }
+}
+
+
+//calculate superfactorial using recursion
+function superFactorial(x){
+    if(x<=1){
+        return 1
+    }
+    else{
+        return x * factorial(x-1) * superFactorial(x-1)
     }
 }
